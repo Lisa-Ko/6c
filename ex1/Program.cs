be7ab3b5adc246feb5002d3b30b525a0e1300fc8
drkop <satanatyn@yandex.ru>
2023-03-06 16:01:10 +0300

    1

---
+++ ex1/Program.cs
@@ -0,0 +1,39 @@
+﻿double[,] CreateInttable()
+{
+    Console.Write("Введите длину  ");
+    int m = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Введите ширину  ");
+    int n = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine();
+    double[,] table = new double[m,n];
+    return  table;
+}
+void Filltable(double[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i,j] =Convert.ToDouble(new Random().Next(100, 1000))/100;
+        }
+    }
+}
+void Printtable(double[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write(" " + table[i,j]);
+        }
+     Console.WriteLine();
+     }
+}
+
+double[,] numbers = CreateInttable();
+Filltable(numbers);
+Console.WriteLine("массив: ");
+Printtable(numbers);
+
