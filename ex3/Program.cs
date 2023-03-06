39f1706ee4fc78840df5e7f3b2b3c92d9e16f29c
drkop <satanatyn@yandex.ru>
2023-03-06 16:04:02 +0300

    3

---
+++ ex3/Program.cs
@@ -0,0 +1,54 @@
+﻿int[,] CreateInttable()
+{
+    Console.Write("Введите длину  ");
+    int m = int.Parse(Console.ReadLine()!);
+
+    Console.Write("Введите ширину  ");
+    int n = int.Parse(Console.ReadLine()!);
+
+    Console.WriteLine();
+    int[,] table = new int[m,n];
+    return  table;
+}
+void Filltable(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            table[i,j] =new Random().Next(0, 10);
+        }
+    }
+}
+void Printtable(int[,] table)
+{
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            Console.Write(" " + table[i,j]);
+        }
+     Console.WriteLine();
+     }
+}
+void PositiveProduct(int[,] table)
+{    
+    for (int i = 0; i < table.GetLength(0); i++)
+    {
+        for (int j = 0; j < table.GetLength(1); j++)
+        {
+            if ((i+1)%2==0 && (j+1)%2==0)
+            {
+                table[i,j]=table[i,j]*table[i,j];
+            }
+        }
+     }
+    Console.WriteLine("измененный массив: ");
+    Printtable(table);
+}
+
+int[,] numbers = CreateInttable();
+Filltable(numbers);
+Console.WriteLine("массив: ");
+Printtable(numbers);
+PositiveProduct(numbers);
